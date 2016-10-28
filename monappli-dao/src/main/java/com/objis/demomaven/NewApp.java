d13cd95d686663f0cc9f4f1066b2cdb02ead6e3c
Christophe <christophe@sentrysoftware.com>
2016-10-28 11:17:36 +0200

    Totally duplicated

--- monappli-dao/src/main/java/com/objis/demomaven/NewApp.java
+++ monappli-dao/src/main/java/com/objis/demomaven/NewApp.java
@@ -2,6 +2,10 @@ package com.objis.demomaven;
 
 public class NewApp {
 
+	public static void main(String[] args) {
+		System.out.println("Hello World!" + isTrue());
+	}
+
     private static boolean isTrue() {
     	return true;
     }
